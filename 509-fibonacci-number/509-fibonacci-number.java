8f7fbdb79c47c9f18106b40527eb620041057bfc
Saksham-2002 <71186616+Saksham-2002@users.noreply.github.com>
2022-07-25 09:35:52 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (94.19%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,25 +1,23 @@
 class Solution {
     public int fib(int n) {
         
-        //Using Memoization
+        
+        //Using Tabulation
         int dp[]= new int[n+1];
         
-        return fibonacci(n, dp);
-    }
-    public int fibonacci(int n, int dp[])
-    {
         if(n<2)
         {
             return n;
         }
-        if(dp[n]!=0)
+        
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2; i<=n; i++)
         {
-            return dp[n];
+            dp[i]=dp[i-2]+dp[i-1];
         }
         
-        int fibn=fibonacci(n-2, dp)+fibonacci(n-1,dp);
-        dp[n]=fibn;
-        
-        return fibn;
+        return dp[n];
     }
 }
